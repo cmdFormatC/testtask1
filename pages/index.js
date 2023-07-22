19d956356ae3d05ace3b5be724f23b71af092211
Bragin Fedor <bragin.fedor@gmail.com>
2023-07-22 13:20:35 +0400

    upd with animationFrame

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,43 @@
 const timerInput = document.querySelector(".timer__input");
 const timerCounter = document.querySelector('.timer__counter');
 
-function startTimer() {
-    let interval;
-    if (interval) {
-        clearInterval(interval);
+let timeInSeconds;
+let end;
+let animationFrameId;
+
+function timer() {
+    const currentTime = Date.now();
+    const remaining = end - currentTime;
+
+    if (remaining <= 0) {
+        timerCounter.textContent = "00:00:00";
+        cancelAnimationFrame(animationFrameId);
+        return;
     }
 
-    let timeInSeconds = parseInt(timerInput.value, 10);
-    let date = new Date();
-    date.setHours(0);
-    date.setMinutes(0);
-    date.setSeconds(timeInSeconds);
+    const date = new Date(remaining);
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+
+    timerCounter.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    animationFrameId = requestAnimationFrame(timer);
+}
+
+function startTimer() {
+    if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+    }
+
+    timeInSeconds = parseInt(timerInput.value, 10);
     if (isNaN(timeInSeconds) || timeInSeconds <= 0) {
         return;
     }
 
     timerInput.value = '';
-    interval = setInterval(function() {
-        if (date.getSeconds() > 0) {
-            date.setSeconds(date.getSeconds() - 1);
-            timerCounter.textContent = date.toTimeString().split(' ')[0];
-        } else {
-            clearInterval(interval);
-            interval = null;
-        }
-    }, 1000);
+    end = Date.now() + timeInSeconds * 1000;
+    timer();
 }
 
 document.querySelector(".timer__button").addEventListener("click", startTimer);
